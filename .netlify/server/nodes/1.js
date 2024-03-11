6476cc4de2d4f1fd3d5d44685b6791bf90571ef1
San126 <sandrarnair@gmail.com>
2024-03-11 17:44:20 +0530

    build2

--- .netlify/server/nodes/1.js
+++ .netlify/server/nodes/1.js
@@ -3,6 +3,6 @@
 export const index = 1;
 let component_cache;
 export const component = async () => component_cache ??= (await import('../entries/fallbacks/error.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/1.DsWFPUWL.js","_app/immutable/chunks/scheduler.COG8yuid.js","_app/immutable/chunks/index.CCnSiE17.js","_app/immutable/chunks/entry.B1kWl_9_.js"];
+export const imports = ["_app/immutable/nodes/1.h9W4ZSz-.js","_app/immutable/chunks/scheduler.COG8yuid.js","_app/immutable/chunks/index.CCnSiE17.js","_app/immutable/chunks/entry.1TRxUfuM.js"];
 export const stylesheets = [];
 export const fonts = [];
